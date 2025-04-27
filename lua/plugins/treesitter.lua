3a2f5b88a3e6a6345484d5427db9eaa1c7ac1062
gui <guizits200@gmail.com>
2025-04-26 21:14:02 -0300

    put treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "windwp/nvim-ts-autotag"
+  },
+  build = ':TSUpdate',
+  config = function()
+    local config = require("nvim-treesitter.configs")
+
+    config.setup({
+      ensure_installed = { "lua", "javascript", "java",
+      "typescript", "html", "css", "tsx", "gitignore" },
+      highlight = { enable = true },
+      autotag = {
+        enable = true
+      }
+    })
+  end
+}
